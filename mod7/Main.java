00f698dca1ebd9222d3764a5582525b528934335
Mateus Coutinho Pacífico <103853362+idkMefius@users.noreply.github.com>
2025-06-06 14:47:30 -0300

    Create Main.java

---
+++ mod7/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Pokemon pokemon = new Pokemon();
+        pokemon.registerNewPokemon("Bulbasaur");
+        pokemon.registerNewPokemon("Ivysaur");
+        pokemon.registerNewPokemon("Venusaur");
+    }
+}
