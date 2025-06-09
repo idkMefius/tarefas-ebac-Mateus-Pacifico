553ea3efc81577dd57664debfb07a05d66d2d4a8
Mateus Coutinho Pacífico <103853362+idkMefius@users.noreply.github.com>
2025-06-09 13:20:35 -0300

    Create Main.java

---
+++ mod9/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int valor1 = 10;
+        System.out.println(valor1);
+        Integer valorWrapper = valor1;
+        System.out.println(valorWrapper);
+    }
+}
