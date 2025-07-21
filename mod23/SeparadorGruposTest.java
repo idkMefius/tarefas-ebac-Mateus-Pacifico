b7b4210926377af55f4596c18bff420ebc7b9e73
Mateus Coutinho Pacífico <103853362+idkMefius@users.noreply.github.com>
2025-07-21 15:27:48 -0300

    Update SeparadorGruposTest.java

--- mod23/SeparadorGruposTest.java
+++ mod23/SeparadorGruposTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
